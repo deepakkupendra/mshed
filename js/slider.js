9726722bb24a157579a4c9e5e8ed44bdf01534ad
deepakkupendra <31625573+deepakkupendra@users.noreply.github.com>
2017-10-22 16:27:59 +0530

    t4
    t4

--- js/slider.js
+++ js/slider.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	$('#prev').on('click', prevSlide);// call function prevSlide
 	
 	// Auto Slider Handler
-	if(autoswitch == true){
+	if(false){
 		setInterval(nextSlide,autoswitch_speed);// call function and value 4000
 	}
 	
